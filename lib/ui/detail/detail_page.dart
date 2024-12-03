0012d5b54098d22c8298844ac7d795eb2399b149
Yoonsuh Kim <blawesomek@gmail.com>
2024-12-03 12:25:58 +0900

    detail_page AppBar

--- lib/ui/detail/detail_page.dart
+++ lib/ui/detail/detail_page.dart
@@ -4,8 +4,29 @@ class DetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        actions: [
+          iconButton(Icons.delete, () {
+            // print('삭제 아이콘 터치함'); 잘 작동하는지 확인용
+          }),
+          iconButton(Icons.edit, () {
+            // print('수정 아이콘 터치함'); 잘 작동하는지 확인용
+          }),
+        ],
+      ),
       body: Text('DetailPage'),
+    );
+  }
+
+  Widget iconButton(IconData icon, void Function() onTap) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 50,
+        height: 50,
+        color: Colors.transparent,
+        child: Icon(icon),
+      ),
     );
   }
 }
